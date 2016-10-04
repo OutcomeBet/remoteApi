c70fa49ce3c8fb1de7502c6f8bf007e9f952f3c1
Scukerman <nngoblin@gmail.com>
2016-10-04 16:17:47 +0300

    Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('default', ['uglify']);
+gulp.task('default', ['compress']);
 
-gulp.task('uglify', function() {
+gulp.task('compress', function() {
 	return gulp.src(['src/remote.js'])
-		.pipe(uglify({mangle: false}))
+		.pipe(uglify({ mangle: false }))
 		.pipe(rename('remote.min.js'))
 		.pipe(gulp.dest('dist'));
 });
--- src/remote.js
+++ src/remote.js
@@ -3,16 +3,16 @@ function RemoteProxy(config) {
 	this.__methodSeparator = config.methodSeparator || '.';
 	this.addMethods(config.methods || {});
 	this.__pendingRequests = [];
-	this.__throttledRequest = _.throttle(this.__doBatchRequest, config.throttle || 0, {leading: false});
+	this.__throttledRequest = _.throttle(this.__doBatchRequest, config.throttle || 0, { leading: false });
 	this.__promiseConverter = config.promiseConverter;
 }
 
 function doRequestServer(requestsData) {
-	var requestBody = JSON.stringify({calls: requestsData});
+	var requestBody = JSON.stringify({ calls: requestsData });
 	return new Promise(function(resolve, reject) {
 		$.ajax({
 			url: this.__url,
-			headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+			headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
 			data: requestBody,
 			type: 'POST',
 			dataType: 'json',
@@ -45,29 +45,28 @@ RemoteProxy.prototype = {
 		var requests = this.__pendingRequests.splice(0, this.__pendingRequests.length);
 		console.log('Running ' + requests.length + ' requests in a batch:', requests);
 
-		var requestsData = _.map(requests, function(request){ return {method: request.method, args: request.args}; });
-		doRequestServer.call(this, requestsData).then(function(responsesData){
-			_.each(responsesData, function(responseData, key){
+		var requestsData = _.map(requests, function(request) { return { method: request.method, args: request.args }; });
+		doRequestServer.call(this, requestsData).then(function(responsesData) {
+			_.each(responsesData, function(responseData, key) {
 				var request = requests[key];
 				if(responseData.success)
 					request.resolver.resolve(responseData.response);
 				else
 					request.resolver.reject(responseData.message || responseData);
 			});
-		}).catch(function(error){
+		}).catch(function(error) {
 			console.error(error);
 		});
 	},
 	__convertObject: function(object, prefix) {
 		var proxy = this;
 		var converted = {};
-		_.each(object, function(value, key, list){
+		_.each(object, function(value, key, list) {
 			if(_.isArray(value))
 				converted[key] = proxy.__convertObject(_.object(value, []), prefix + key + proxy.__methodSeparator);
 			else if(_.isObject(value))
 				converted[key] = proxy.__convertObject(value, prefix + key + proxy.__methodSeparator);
-			else
-			{
+			else {
 				var method = prefix + key;
 				converted[key] = function() {
 					var promise = proxy.__addPendingRequest(method, arguments);
@@ -81,10 +80,10 @@ RemoteProxy.prototype = {
 	call: function(method, args) {
 		return this.__addPendingRequest(method, args);
 	},
-	addMethods: function(methods, prefix){
+	addMethods: function(methods, prefix) {
 		_.extend(this, this.__convertObject(methods || {}, prefix || ''));
 	},
-	createProxy: function(methods, prefix){
+	createProxy: function(methods, prefix) {
 		return this.__convertObject(methods, prefix || '')
 	}
 };
